90f9cd6899b6bcf1615e50c5850e3a8513840a76
DatoneX <cgomwe@ymail.com>
2022-03-21 22:18:02 +0000

    Customse admin view and change media file extensions

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Brand, Category, Product
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Brand)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "sku",
+        "name",
+        "brand",
+        "category",
+        "price",
+        "rating",
+        "image",
+    )
+
+    ordering = ("sku",)
+
+
+@admin.register(Category)
+@admin.register(Brand)
+class DatabaseAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "db_name",
+    )
+
+    ordering = ("id",)
