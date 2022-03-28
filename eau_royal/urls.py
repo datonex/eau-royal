22f8ef69fea36f79dc01b8bcb603e10f89547dc4
DatoneX <cgomwe@ymail.com>
2022-03-29 00:32:23 +0100

    Add all products view, template and styling

--- eau_royal/urls.py
+++ eau_royal/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("home.urls")),
+    path("products/", include("products.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """View function that returns all products, including search and sorting queries"""
+
+    template = "products/products.html"
+
+    products_list = Product.objects.all()
+    paginator = Paginator(products_list, 60)
+    page_number = request.GET.get("page")
+
+    try:
+        products = paginator.page(page_number)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    context = {
+        "products": products,
+    }
+
+    return render(request, template, context)
