f8f3736e1f4bb04cecac138e9f2d629b5454365e
DatoneX <cgomwe@ymail.com>
2022-04-05 02:12:04 +0100

    Add homepage filter queries

---
+++ home/static/home/img/models.py
@@ -0,0 +1,62 @@
+from unicodedata import name
+from django.db import models
+import glob, os
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    db_name = models.CharField(max_length=254)
+    name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.db_name
+
+    def get_friendly_name(self):
+        return self.name
+
+
+class Brand(models.Model):
+    class Meta:
+        verbose_name_plural = "Brands"
+
+    db_name = models.CharField(max_length=254)
+    name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.db_name
+
+    def get_friendly_name(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    GENDER = (
+        ("Unisex", "Unisex"),
+        ("Men", "Men"),
+        ("Women", "Women"),
+    )
+
+    name = models.CharField(max_length=254)
+    brand = models.ForeignKey("Brand", null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True, unique=True)
+    category = models.ForeignKey(
+        "Category", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    gender = models.CharField(max_length=6, choices=GENDER)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    has_discount = models.BooleanField(default=False, null=True, blank=True)
+    has_subscription = models.BooleanField(default=False, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True, default=0
+    )
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    scent = models.CharField(max_length=50, null=True, blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
+from dis import dis
+from itertools import product
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q
+from django.db.models import Q, F
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from .models import Product
+from .models import Product, Category, Gender
 
 
 def all_products(request):
@@ -13,8 +15,20 @@ def all_products(request):
     products = Product.objects.all()
 
     query = None
+    categories = None
+    genders = None
 
     if request.GET:
+        if "category" in request.GET:
+            categories = request.GET["category"].split(",")
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+        if "gender" in request.GET:
+            genders = request.GET["gender"].split(",")
+            products = products.filter(gender__db_name__in=genders)
+            genders = Gender.objects.filter(db_name__in=genders)
+        if "has_discount" in request.GET:
+            products = products.filter(has_discount=True)
         if "q" in request.GET:
             query = request.GET["q"]
             if not query:
@@ -42,6 +56,7 @@ def all_products(request):
     context = {
         "products": products,
         "search_term": query,
+        "homepage_filters": categories,
     }
 
     return render(request, template, context)
