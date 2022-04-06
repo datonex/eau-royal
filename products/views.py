5fb3a2b642c04632aa490688fa63fe36d7c4bc90
DatoneX <cgomwe@ymail.com>
2022-04-06 16:50:56 +0100

    Add sorting to medium screens and over

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from dis import dis
-from itertools import product
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q, F
+from django.db.models import Q
+from django.db.models.functions import Lower
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 from .models import Product, Category, Gender
 
 
@@ -17,8 +17,24 @@ def all_products(request):
     query = None
     categories = None
     genders = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if "sort" in request.GET:
+            sortkey = request.GET["sort"]
+            sort = sortkey
+            if sortkey == "name":
+                sortkey = "lower_name"
+                products = products.annotate(lower_name=Lower("name"))
+            if sortkey == "category":
+                sortkey = "category__name"
+            if "direction" in request.GET:
+                direction = request.GET["direction"]
+                if direction == "desc":
+                    sortkey = f"-{sortkey}"
+            products = products.order_by(sortkey)
+
         if "category" in request.GET:
             categories = request.GET["category"].split(",")
             products = products.filter(category__name__in=categories)
@@ -29,6 +45,7 @@ def all_products(request):
             genders = Gender.objects.filter(db_name__in=genders)
         if "has_discount" in request.GET:
             products = products.filter(has_discount=True)
+
         if "q" in request.GET:
             query = request.GET["q"]
             if not query:
@@ -42,6 +59,8 @@ def all_products(request):
                 | Q(category__name__icontains=query)
             )
             products = products.filter(queries)
+
+    current_sorting = f"{sort}_{direction}"
 
     page_number = request.GET.get("page", 1)
     paginator = Paginator(products, 60)
@@ -57,6 +76,7 @@ def all_products(request):
         "products": products,
         "search_term": query,
         "homepage_filters": categories,
+        "current_sorting": current_sorting,
     }
 
     return render(request, template, context)
