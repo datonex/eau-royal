af95b79327fd9ea0a16a23b208acc011a878fb45
DatoneX <cgomwe@ymail.com>
2022-04-11 19:46:00 +0100

    Add product item to bag session functionality

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from django.shortcuts import render
 
 
 def view_bag(request):
-    """View to return the shopping bag content"""
+    """View to return the shopping bag contents page"""
 
     template = "bag/bag.html"
     return render(request, template)
+
+
+def add_to_bag(request, item_id):
+    """Add a new item to the shopping bag"""
+
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
+    bag = request.session.get("bag", {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session["bag"] = bag
+    print(request.session["bag"])
+    return redirect(redirect_url)
