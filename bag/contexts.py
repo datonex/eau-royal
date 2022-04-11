a59e444e1cbaa4f79b67c5a926891fbcf75fd24d
DatoneX <cgomwe@ymail.com>
2022-04-11 23:18:38 +0100

    Update context processor and show current bag total

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,6 +9,19 @@ def bag_contents(request):
     bag_items = []
     sub_total = 0
     product_count = 0
+    bag = request.session.get("bag", {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        sub_total += quantity * product.price
+        product_count += quantity
+        bag_items.append(
+            {
+                "item_id": item_id,
+                "quantity": quantity,
+                "product": product,
+            }
+        )
 
     if sub_total >= settings.DISCOUNT_THRESHOLD:
         discount = sub_total * Decimal(settings.DISCOUNT_PERCENTAGE / 100)
@@ -15,7 +30,7 @@ def bag_contents(request):
         discount = 0
         discount_delta = 0
 
-    total = discount - sub_total
+    total = discount + sub_total
 
     context = {
         "bag_items": bag_items,
