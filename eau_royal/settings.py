e505edac6f5a0c058d3b6db6e2961b3513c224eb
Datonex <cgomwe@ymail.com>
2022-04-20 12:27:04 +0100

    Fix deployment to heroku attempt 2

--- eau_royal/settings.py
+++ eau_royal/settings.py
@@ -21,7 +21,6 @@ if os.path.isfile("env.py"):
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-development = os.environ.get("DEVELOPMENT")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
