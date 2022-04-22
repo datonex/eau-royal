3aceb9879e131ea01e08d30656768eb2e0d527d8
Datonex <cgomwe@ymail.com>
2022-04-22 15:23:52 +0100

    Set up checkout page, views, urls, template and css

--- eau_royal/urls.py
+++ eau_royal/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path("", include("home.urls")),
     path("products/", include("products.urls")),
     path("bag/", include("bag.urls")),
+    path("checkout/", include("checkout.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
