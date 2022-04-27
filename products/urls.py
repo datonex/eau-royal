dc06ebf7025ed46447e02345842f9dbbc662e13e
Datonex <cgomwe@ymail.com>
2022-04-27 23:54:04 +0100

    Add product to database functionality for admin

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.all_products, name="products"),
     path("<int:product_id>/", views.product_detail, name="product_detail"),
+    path("add/", views.add_product, name="add_product"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,13 @@
 from django.forms import ValidationError
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q, Max, Min
 from django.db.models.functions import Lower
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Brand, Product, Category, Gender
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -159,6 +161,34 @@ def product_detail(request, product_id):
 
     context = {
         "product": product,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def add_product(request):
+    """Add a product to the store"""
+    if not request.user.is_superuser:
+        messages.error(request, "Sorry, only store owners can do that.")
+        return redirect(reverse("home"))
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, "Successfully added product!")
+            return redirect(reverse("product_detail", args=[product.id]))
+        else:
+            messages.error(
+                request, ("Failed to add product. " "Please ensure the form is valid.")
+            )
+    else:
+        form = ProductForm()
+
+    template = "products/add_product.html"
+    context = {
+        "form": form,
     }
 
     return render(request, template, context)
