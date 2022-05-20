51fd2b363bdcde0d35bc295654dae210fec6a1e3
Datonex <cgomwe@ymail.com>
2022-05-20 18:15:16 +0100

    Add basic form validation at checkout

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,8 +1,21 @@
 from django import forms
+from django.forms.widgets import TextInput
+
 from .models import Order
 
 
+class PhoneInput(TextInput):
+    """Define a telephone input class for use with phone numbers"""
+
+    input_type = "tel"
+
+
 class OrderForm(forms.ModelForm):
+    """
+    Form Class that renders a form to the user so that their order
+    can be sent to the database
+    """
+
     class Meta:
         model = Order
         fields = (
@@ -16,6 +29,44 @@ class OrderForm(forms.ModelForm):
             "postcode",
             "country",
         )
+
+        widgets = {
+            "full_name": forms.TextInput(
+                attrs={
+                    "pattern": "/^[a-zA-Z-']+ [a-zA-Z-']+$/",
+                    "title": "Please enter your FULL name",
+                    "autocomplete": "name",
+                }
+            ),
+            "email": forms.EmailInput(
+                attrs={
+                    "pattern": "/^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z]+$/",
+                    "title": "Please enter your email in the format: email@example.com",
+                    "autocomplete": "email",
+                }
+            ),
+            "phone_number": PhoneInput(
+                attrs={
+                    "pattern": """/^\+[1-9]\d{2}\d{10}|\+[1-9][0-9]\d{10}|0?1?8?\d{4,15}$/""",
+                    "title": """Please enter a valid phone number in the format: 0123456789 or +12345678912""",
+                    "autocomplete": "tel",
+                }
+            ),
+            "street_address1": forms.TextInput(
+                attrs={
+                    "pattern": "/^[a-zA-Z0-9-.']+ [a-zA-Z0-9-.']+ [a-zA-Z0-9-.']+$/",
+                    "title": "Please enter a valid address",
+                    "autocomplete": "address-line1",
+                }
+            ),
+            "town_or_city": forms.TextInput(
+                attrs={
+                    "pattern": "/^[a-zA-Z-.' ]+$/",
+                    "title": "Please enter a valid town or city",
+                    "autocomplete": "address-level1",
+                }
+            ),
+        }
 
     def __init__(self, *args, **kwargs):
         """
